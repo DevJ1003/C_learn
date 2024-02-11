43e7fcca60ae64014e4d4883cf1d098f76659d35
DevJ1003 <devjoshi1384@gmail.com>
2024-02-12 00:14:58 +0530

    commit

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int MyNum = 0;
+
+    printf("Please enter the number: ");
+    scanf("%d", &MyNum);
+    printf("You entered %d", MyNum);
+
+    return 0;
+}
